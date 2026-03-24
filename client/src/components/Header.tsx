9a70ac3bdb0e71cf54b9396e5eae69ce3c82a692
Anton BV <a.biletskiy@gmail.com>
2026-03-24 23:37:58 +0100

    Show Telegram monitor handle on mobile header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -347,7 +347,7 @@ export function Header({ onCreateOption: _onCreateOption, onOpenLogin: _onOpenLo
                       className="h-8 gap-1.5 px-2"
                     >
                       <Send className="h-3.5 w-3.5" />
-                      <span className="hidden sm:inline text-[11px]">{monitorHandle}</span>
+                      <span className="max-w-[120px] truncate text-[11px]">{monitorHandle}</span>
                     </Button>
                   </DropdownMenuTrigger>
                   <DropdownMenuContent align="end">
